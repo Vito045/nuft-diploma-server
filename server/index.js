e216c1a24d183f690a28eab9ee357ccbbf7aa193
Vito045 <vito045@icloud.com>
2020-06-11 20:47:18 +0300

    dsfsdf

--- server/index.js
+++ server/index.js
@@ -18,8 +18,9 @@ const { ObjectID } = mongodb;
 const User = require('./models/userModel');
 const Chat = require('./models/chatModel');
 
-const port = process.env.PORT || 3001;
-const PORT = process.env.PORT || 3001;
+// const port = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 // const PORT = 3001;
 const INDEX = '/index.html';
 
