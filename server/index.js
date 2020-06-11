71f1bc167541d2f193fa19b8706d0f41b011aa29
Vito045 <vito045@icloud.com>
2020-06-11 19:39:52 +0300

    fsdfsdf

--- server/index.js
+++ server/index.js
@@ -18,15 +18,15 @@ const { ObjectID } = mongodb;
 const User = require('./models/userModel');
 const Chat = require('./models/chatModel');
 
-// const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 const PORT = process.env.PORT || 3000;
 // const PORT = 3001;
 const INDEX = '/index.html';
 
-// const server = http.createServer(app);
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = http.createServer(app);
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 const io = soketio(server);
 
 const sockets = {};
@@ -578,4 +578,4 @@ io.on('connection', async (socket) => {
   delete sockets[socket.userId];
 });
 
-// server.listen(port, () => console.log('Server is up on port', port));
+server.listen(port, () => console.log('Server is up on port', port));
