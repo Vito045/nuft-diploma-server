6988beea99e95c1916228ba508778bc25377b423
Vito045 <vito045@icloud.com>
2020-06-15 17:09:50 +0300

    sdfsf

--- index.js
+++ index.js
@@ -589,7 +589,7 @@ io.on('connection', async (socket) => {
 
       // user.chats.forEach(chat => socket.to(chat._id).emit('updateUserInChat', { chatId: chat._id, user });)
       const u = await User.findById(user._id);
-      callback(user, { u, user });
+      callback(user);
     } catch (err) {
       console.log(err);
     }
